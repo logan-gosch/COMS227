6323ba68a513534eda449875ed7477c66aca3aef
logan-gosch <logan.gosch.temp@gmail.com>
2023-10-25 14:38:59 -0500

    chore

--- src/mini2/Runner.java
+++ src/mini2/Runner.java
@@ -1,26 +1,26 @@
-package mini2;
-
-import speccheck.SpecCheck;
-
-import javax.swing.*;
-
-public class Runner {
-    public static void main(String args[])
-    {
-        {
-            // GUI dialogs are used, so we need to run on the event thread.
-            SwingUtilities.invokeLater( new Runnable()
-            {
-                public void run()
-                {
-                    SpecCheck.testAndZip(
-                            SpecChecker.class,
-                            "SUBMIT_THIS_mini2",
-                            "mini2",
-                            new String[]{"src/mini2/OneThousandOneArraybianNights.java"}
-                    );
-                }
-            });
-        }
-    }
-}
+//package mini2;
+//
+//import speccheck.SpecCheck;
+//
+//import javax.swing.*;
+//
+//public class Runner {
+//    public static void main(String args[])
+//    {
+//        {
+//            // GUI dialogs are used, so we need to run on the event thread.
+//            SwingUtilities.invokeLater( new Runnable()
+//            {
+//                public void run()
+//                {
+//                    SpecCheck.testAndZip(
+//                            SpecChecker.class,
+//                            "SUBMIT_THIS_mini2",
+//                            "mini2",
+//                            new String[]{"src/mini2/OneThousandOneArraybianNights.java"}
+//                    );
+//                }
+//            });
+//        }
+//    }
+//}
